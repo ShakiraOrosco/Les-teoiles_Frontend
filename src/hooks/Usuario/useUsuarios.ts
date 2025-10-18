113f39d003b0e6c6e1d2ec9b6b18303c9f558ffe
MariolyTintaya <tintayaramosmarioly@gmail.com>
2025-10-17 20:53:41 -0400

    CRU Usuarios

--- src/hooks/Usuario/useUsuarios.ts
+++ src/hooks/Usuario/useUsuarios.ts
@@ -1,29 +1,26 @@
 import { useEffect, useState, useCallback } from "react";
-import { getUsuarios, createUsuario } from "../../services/usuario/usuarioService";
+import { getUsuarios, createUsuario, updateUsuario} from "../../services/usuario/usuarioService";
 import { Usuario } from "../../types/Usuarios/usuario";
+
 export const useUsuarios = () => {
-    const [usuarios, setUsuarios] = useState<Usuario[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
+  const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-    const fetchUsuarios = useCallback(async () => {
-        setLoading(true);
-        setError(null);
-
-        try {
-            const data = await getUsuarios();
-            setUsuarios(data);
-        } catch (err) {
-            setError("Error al cargar usuarios");
-            setUsuarios([]);
-        } finally {
-            setLoading(false);
-        }
-    }, []);
-
-    useEffect(() => {
-        fetchUsuarios();
-    }, [fetchUsuarios]);
+  const fetchUsuarios = async () => {
+    try {
+      setLoading(true);
+      const data = await getUsuarios();
+      setUsuarios(data);
+    } catch (err) {
+      setError("Error al cargar usuarios");
+    } finally {
+      setLoading(false);
+    }
+  };
+   useEffect(() => {
+    fetchUsuarios();
+  }, [fetchUsuarios]);
 
     // === Nuevo: crear usuario ===
     const addUsuario = async (nuevo: Usuario) => {
@@ -34,6 +31,18 @@ export const useUsuarios = () => {
             throw new Error("Error al crear usuario");
         }
     };
+    
+  // === Nuevo: editar usuario ===
+  const editarUsuario = async (id_usuario: number, datos: Partial<Usuario>) => {
+    try {
+      await updateUsuario(id_usuario, datos);
+      await fetchUsuarios(); // recargar lista
+    } catch (error) {
+      console.error("Error al editar usuario:", error);
+    }
+  };
 
-    return { usuarios, loading, error, refetch: fetchUsuarios, addUsuario };
+    return { usuarios, loading, error, addUsuario,editarUsuario, refetch: fetchUsuarios  };
+
+    
 };
--- src/services/usuario/usuarioService.ts
+++ src/services/usuario/usuarioService.ts
@@ -13,4 +13,11 @@ export const createUsuario = async (nuevoUsuario: Partial<Usuario>) => {
   const response = await api.post("/usuarios/crear/", nuevoUsuario);
   return response.data;
 };
-
+// * === SERVICIO PARA EDITAR UN USUARIO === * //
+export const updateUsuario = async (
+  id_usuario: number,
+  datosActualizados: Partial<Usuario>
+) => {
+  const response = await api.put(`/usuarios/${id_usuario}/update/`, datosActualizados);
+  return response.data;
+};
